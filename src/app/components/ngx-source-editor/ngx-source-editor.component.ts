4a72c592505dd54e3a11fd663d5661ac19e06a76
confusement <sudhir18267@iiitd.ac.in>
2021-01-19 15:43:32 +0530

    LayoutFixed

--- src/app/components/ngx-source-editor/ngx-source-editor.component.ts
+++ src/app/components/ngx-source-editor/ngx-source-editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild , AfterViewInit} from '@angular/core';
 
 @Component({
   selector: 'app-ngx-source-editor',
@@ -8,17 +8,39 @@ import { Component, OnInit } from '@angular/core';
 export class NgxSourceEditorComponent implements OnInit {
 
   constructor() { }
-  public code : String = "HAHAHA\nHAHAHA\nHAHAHA\nHAHAHA\nHAHAHA\nHAHAHA\nHAHAHA\nHAHAHA\nHAHAHA\nHAHAHA\nHAHAHA\nHAHAHA\n";
+  public content : String = "HAHAHA\nHAHAHA\nHAHAHA\nHAHAHA\nHAHAHA\nHAHAHA\nHAHAHA\nHAHAHA\nHAHAHA\nHAHAHA\nHAHAHA\nHAHAHA\n";
   public labels : String = "";
+  @ViewChild("code") editorElem :any;
+  ngAfterViewInit() :void{
+    console.log(this.editorElem);
+  }
   ngOnInit(): void {
     for(var i:number =0;i<37;i++)
-    {
-      this.labels += i.toString() + ' ';
+    { 
+      this.labels += i.toString() + '<br>';
     }
     
   }
   public updateLabels(event : any) :void{
 
+  }
+  public onInput(event:any):void{
+    console.log(this.editorElem.nativeElement.innerHTML);  
+  }
+  public onPaste(event :any):void{
+    let clipboardData = event.clipboardData;
+    let pastedText = clipboardData.getData('text/plain');
+    clipboardData.setData('text', pastedText);
+    event.preventDefault();
+    document.execCommand('inserttext', false, pastedText);
+    // console.log(this.content);
+  }
+  public onKeydown(event :any):void{
+    if(event.ctrlKey){
+      if(event.key==','){
+        
+      }
+    }
   }
 
 }
